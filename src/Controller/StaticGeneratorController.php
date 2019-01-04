b03be47f779bb3ada4e6b91fe296b0b0797e498e
vagrant <vagrant@d8.local>
2019-01-04 12:24:02 +0000

    Added settings for esi blocks, esi sg-esi, and render_method.

--- src/Controller/StaticGeneratorController.php
+++ src/Controller/StaticGeneratorController.php
@@ -18,11 +18,12 @@ class StaticGeneratorController extends ControllerBase {
   public function sgTest() {
     $build = [
       //'#markup' => \Drupal::service('static_generator')->generatePage('/node/158364'),
+      '#markup' => \Drupal::service('static_generator')->generatePage('/patent'),
       //'#markup' => \Drupal::service('static_generator')->generatePage('/node/187833'),
       //'#markup' => \Drupal::service('static_generator')->generatePage('/patent', FALSE, TRUE),
       //'#markup' => \Drupal::service('static_generator')->generateNodes('page'),
       //'#markup' => \Drupal::service('static_generator')->generatePages(),
-      '#markup' => \Drupal::service('static_generator')->generateNodes('bio'),
+      //'#markup' => \Drupal::service('static_generator')->generateNodes('bio'),
       //'#markup' => \Drupal::service('static_generator')->generateBlocks(TRUE),
     ];
     return $build;
